48dc7be2984876a7f9ddc935a6be0f5e04aad9b7
Saurav Kaintura <sauravkaintura49041@gmail.com>
2026-04-25 17:11:29 +0530

    initial commit - initialize frontend with React, Vite, and Tailwind CSS and backend with modules and server.js setup with mongo db

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+const app = require('./src/app.js')
+const connectDB = require('./src/config/db.js')
+
+connectDB()
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT,()=>{
+    console.log(`SERVER IS RUNNING ON PORT ${PORT}`)
+})
---
+++ backend/src/app.js
@@ -0,0 +1,14 @@
+dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+
+
+
+module.exports = app;
