b38ee9d8c0af2942503648c8f9a07ca96545ea61
Saurav Kaintura <sauravkaintura49041@gmail.com>
2026-05-26 17:30:36 +0530

    .

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,8 @@ const app = require('./src/app.js')
 const connectDB = require('./src/config/db.js')
 require('./src/config/redis.js')
 
-
 connectDB()
 const PORT = process.env.PORT || 5000;
-
 
 app.listen(PORT,()=>{
     console.log(`SERVER IS RUNNING ON PORT ${PORT}`)
